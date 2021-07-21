ea3b10ac1f91f84b59e518f90ae7f0b7735d0a03
Heena <heenagoyal65@gmail.com>
2021-07-21 10:57:55 +0530

    add pickup nd other detail app

---
+++ mydemo/PickList/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core import validators
+# Create your models here.
+`id`, `picklist_key`, `sub_picklist_id`, `picklist_item_key`, `picklist_parent_id`, `is_active`, `created_at`, `updated_at`, `deleted_at`
--- mydemo/myusers/models.py
+++ mydemo/myusers/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core import validators
 import os
+from address.models import Country, State, City
 
 # Create your models here.
 GENDER_FIELD_CHOICES = (
@@ -27,6 +28,31 @@ class UserProfile(models.Model):
 	dob = models.DateField(verbose_name="Date of birth")
 	created_at = models.DateTimeField(verbose_name="Date (created)", auto_now_add=True)
 	updated_at = models.DateTimeField(verbose_name="Date (updated)", auto_now=True)
+	time_of_birth = models.TimeField(verbose_name="Time of birth")
+	place_of_birth = models.ForeignKey(City, verbose_name='Place of birth', related_name="placeOfBirth", null=False, on_delete=models.CASCADE)
+	address = models.CharField(verbose_name="Address", null=False, max_length=255)
+	country = models.ForeignKey(Country, verbose_name='Country', related_name="country", null=False, on_delete=models.CASCADE)
+	state = models.ForeignKey(State, verbose_name='State', related_name="state", null=False, on_delete=models.CASCADE)
+	city = models.ForeignKey(City, verbose_name='City', related_name="city", null=False, on_delete=models.CASCADE)
+	pincode = models.CharField(verbose_name="Pincode", null=False, max_length=20)
+	phone_no = models.IntegerField(verbose_name="Phone Number", null=False, max_length=15)
+	height = models.FloatField(verbose_name="Height", null=False, max_length=15)
+	weight = models.FloatField(verbose_name="Weight", null=False, max_length=15)
+	# relegion = models.ForeignKey(verbose_name="Phone Number", null=False, max_length=15)
+	# mothertoung = models.ForeignKey(verbose_name="Phone Number", null=False, max_length=15)
+	# cast = models.ForeignKey(verbose_name="Phone Number", null=False, max_length=15)
+	# sub_cast = models.ForeignKey(verbose_name="Phone Number", null=False, max_length=15)
+	# gotra = models.ForeignKey(verbose_name="Phone Number", null=False, max_length=15)
+	# manglik = models.IntegerField(verbose_name="Phone Number", null=False, max_length=15)
+	# material_status = models.IntegerField(verbose_name="Phone Number", null=False, max_length=15)
+	# smoke = models.CharField(verbose_name="Phone Number", null=False, max_length=15)
+	# drink = models.CharField(verbose_name="Phone Number", null=False, max_length=15)
+	# dite = models.CharField(verbose_name="Phone Number", null=False, max_length=15)
+	# bio = models.TextField(verbose_name="Phone Number", null=False, max_length=15)
+	# education = models.ForeignKey(verbose_name="Phone Number", null=False, max_length=15)
+	# occupaion = models.ForeignKey(verbose_name="Phone Number", null=False, max_length=15)
+	# salary = models.IntegerField(verbose_name="Phone Number", null=False, max_length=15)
+
 
 
 	class Meta:
